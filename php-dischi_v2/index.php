7105786e86bef34a5fd17678ed8004a161da4ad3
Daniele Praino <daniele.praino@gmail.com>
2019-06-19 19:12:31 +0200

    php-dischi_v2 done minor improvements

--- php-dischi_v2/index.php
+++ php-dischi_v2/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link href="node_modules/@fortawesome/fontawesome-free/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="public/css/main.css">
     <title>php-dischi</title>
   </head>
@@ -11,15 +12,15 @@
       <nav>
         <div class="nav-container">
           <div class="logo">
-            <h2>Album viewer</h2>
+            <h2>Album viewer <i class="fas fa-compact-disc"></i><i class="fas fa-music"></i></h2>
           </div>
           <div class="nav-select">
             <div class="filter-generic">
               <span>Ordina album per:</span>
               <select class="generic-select">
                 <option value="0" selected disabled>Seleziona</option>
-                <option value="release">data di uscita</option>
-                <option value="alphabetic">ordine alfabetico</option>
+                <option value="release">data di uscita (1->9)</option>
+                <option value="alphabetic">ordine alfabetico (A->Z)</option>
               </select>
             </div>
             <div class="filter-band">
